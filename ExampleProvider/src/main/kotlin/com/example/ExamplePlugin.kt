90d7383245649cc6eca97902e90b4db19d137efb
Eyüp Aslan <eyupaslnn00@gmail.com>
2026-09-13 22:07:41 +0300

    Add HDFilmCehennemi provider and setup multi-provider architecture

--- ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExamplePlugin.kt
@@ -14,6 +14,7 @@ class ExamplePlugin: Plugin() {
 
         // All providers should be added in this manner
         registerMainAPI(YouTubeProvider())
+        registerMainAPI(HdFilmCehennemiProvider())
 
         openSettings = {
             val frag = BlankFragment(this)
